5232a9cdc5a9439a80e3fc0a7ccb167e5f254a0d
z30009938 <zhonglei26@huawei.com>
2022-01-15 17:13:43 +0800

    reformat program to springboot

---
+++ sermant-plugins/sermant-hercules/ngrinder/ngrinder-controller-service/src/main/java/com/huawei/argus/config/UserFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2022-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huawei.argus.config;
+
+import org.ngrinder.model.Role;
+import org.ngrinder.model.User;
+
+/**
+ * 功能描述：
+ *
+ * @author zl
+ * @since 2022-01-14
+ */
+public class UserFactory {
+    public static User newInstance() {
+        User user = new User();
+        user.setUserName("admin");
+        user.setRole(Role.ADMIN);
+        user.setUserLanguage("en");
+        user.setId(1L);
+        user.setUserId("admin");
+        user.setTimeZone("Asia/Shanghai");
+        user.setExternal(true);
+        user.setEnabled(true);
+        return user;
+    }
+}
