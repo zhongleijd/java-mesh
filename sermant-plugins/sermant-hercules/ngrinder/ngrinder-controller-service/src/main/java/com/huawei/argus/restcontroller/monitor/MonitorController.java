ebc60cea3d158f0058ef67be72bece17a84a63e4
zhongleijd <king_zhonglei@163.com>
2022-02-24 18:04:01 +0800

    删除多余代码

--- sermant-plugins/sermant-hercules/ngrinder/ngrinder-controller-service/src/main/java/com/huawei/argus/restcontroller/monitor/MonitorController.java
+++ sermant-plugins/sermant-hercules/ngrinder/ngrinder-controller-service/src/main/java/com/huawei/argus/restcontroller/monitor/MonitorController.java
@@ -16,12 +16,9 @@
 
 package com.huawei.argus.restcontroller.monitor;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.huawei.argus.restcontroller.monitor.dto.MonitorHostDTO;
 import com.huawei.argus.service.influxdb.IMonitorService;
 import com.huawei.argus.service.influxdb.metric.tree.impl.RootMetricNode;
-import org.ngrinder.service.IPerfTestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,9 +44,6 @@ public class MonitorController {
     @Autowired
     private IMonitorService monitorService;
 
-    @Autowired
-    private IPerfTestService perfTestService;
-
     /**
      * global influxdb bucket
      */
@@ -74,34 +68,5 @@ public class MonitorController {
         monitorModel.setSuccess(true);
         monitorModel.setData(allMonitorData);
         return monitorModel;
-    }
-
-    /**
-     * 查询需要监控的主机是否需要获取jvm数据
-     *
-     * @param monitorHostDTO 查询传入的参数信息
-     * @param monitorHosts   系统查询到的主机信息
-     */
-    private void initJvmConfig(MonitorHostDTO monitorHostDTO, JSONArray monitorHosts) {
-        if (monitorHostDTO == null || StringUtils.isEmpty(monitorHostDTO.getHost()) || StringUtils.isEmpty(monitorHostDTO.getIp())) {
-            throw new RuntimeException("Host or ip can not be empty for monitoring.");
-        }
-        if (monitorHosts == null || monitorHosts.isEmpty()) {
-            throw new RuntimeException("Can not found this host in config of this test.");
-        }
-        for (int i = 0; i < monitorHosts.size(); i++) {
-            JSONObject monitorHost = monitorHosts.getJSONObject(i);
-            if (!monitorHostDTO.getHost().equals(monitorHost.getString(MonitorHostKey.HOST.getServerKey()))) {
-                continue;
-            }
-            if (!monitorHostDTO.getIp().equals(monitorHost.getString(MonitorHostKey.IP.getServerKey()))) {
-                continue;
-            }
-            Boolean isMonitorJvm = monitorHost.getBoolean(MonitorHostKey.IS_MONITOR_JVM.getServerKey());
-            String jvmType = monitorHost.getString(MonitorHostKey.JVM_TYPE.getServerKey());
-            monitorHostDTO.setJvmType(jvmType);
-            monitorHostDTO.setIsMonitorJvm(isMonitorJvm);
-            break;
-        }
     }
 }
