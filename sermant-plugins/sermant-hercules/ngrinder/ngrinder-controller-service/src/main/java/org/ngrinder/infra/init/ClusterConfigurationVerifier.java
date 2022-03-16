c57630b6ee3560ba9bbb145def1827f57556df4e
zhongleijd <king_zhonglei@163.com>
2022-03-16 20:12:50 +0800

    修改ngrinder集群部署失败的问题

--- sermant-plugins/sermant-hercules/ngrinder/ngrinder-controller-service/src/main/java/org/ngrinder/infra/init/ClusterConfigurationVerifier.java
+++ sermant-plugins/sermant-hercules/ngrinder/ngrinder-controller-service/src/main/java/org/ngrinder/infra/init/ClusterConfigurationVerifier.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package org.ngrinder.infra.init;
 
@@ -58,7 +58,7 @@ public class ClusterConfigurationVerifier {
 	 */
 	private void checkDB() {
 		String db = config.getDatabaseProperties().getProperty(DatabaseConstants.PROP_DATABASE_TYPE).toLowerCase();
-		if (!db.equals("cubrid")) {
+		if (!db.equals("cubrid") && !db.equals("mysql") && !db.equals("gaussdb")) {
 			final String dbURL = config.getDatabaseProperties().getProperty(DatabaseConstants.PROP_DATABASE_URL, "");
 			checkState(dbURL.startsWith("tcp://"), "Wrong database.url configuration " + dbURL + "\n" +
 					"When cluster mode is enabled, embedded H2 db can not be used. Use cubrid or Use H2 TCP server");
