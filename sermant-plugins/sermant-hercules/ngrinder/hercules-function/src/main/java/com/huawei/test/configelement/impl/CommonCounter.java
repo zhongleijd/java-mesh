87d95553470ed560ca3796c09d747486610c0862
zhongleijd <king_zhonglei@163.com>
2022-03-30 15:26:28 +0800

    添加计数器重复取数逻辑

--- sermant-plugins/sermant-hercules/ngrinder/hercules-function/src/main/java/com/huawei/test/configelement/impl/CommonCounter.java
+++ sermant-plugins/sermant-hercules/ngrinder/hercules-function/src/main/java/com/huawei/test/configelement/impl/CommonCounter.java
@@ -20,10 +20,11 @@ import com.huawei.test.configelement.Counter;
 import com.huawei.test.configelement.config.CounterConfig;
 import com.huawei.test.configelement.service.ExecuteTimesInfo;
 import com.huawei.test.exception.FunctionException;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -33,80 +34,99 @@ import java.util.Locale;
  * @since 2021-12-09
  */
 public class CommonCounter extends Counter {
-	/**
-	 * 日志
-	 */
-	private static final Logger LOGGER = LoggerFactory.getLogger(CommonCounter.class);
+    /**
+     * 日志
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommonCounter.class);
 
-	/**
-	 * config配置
-	 */
-	private CounterConfig counterConfig;
+    /**
+     * config配置
+     */
+    private CounterConfig counterConfig;
 
-	@Override
-	public void initConfig(CounterConfig config) {
-		this.counterConfig = config;
-	}
+    /**
+     * 缓存该线程能使用的数字
+     */
+    private final List<Integer> cacheNumber = new ArrayList<>();
 
-	@Override
-	public boolean isConfigValid() {
-		if (counterConfig == null) {
-			LOGGER.error("The counter config is null.");
-			return false;
-		}
-		return counterConfig.getSharingMode() != null;
-	}
+    /**
+     * 当前取数的游标
+     */
+    private int cursor = 0;
 
-	@Override
-	public String nextNumber() {
-		if (!isConfigValid()) {
-			throw new FunctionException("The counter config is invalid.");
-		}
-		int nextValue;
-		try {
-			nextValue = nextValue();
-		} catch (ArithmeticException arithmeticException) {
-			LOGGER.error("Get next value fail when counter, reason:{}", arithmeticException.getMessage());
-			throw new FunctionException("Get next value fail when counter");
-		}
-		Integer maxValue = counterConfig.getMaxValue();
-		if(maxValue != null && nextValue > maxValue) {
-			LOGGER.error("Over max value:{}", counterConfig.getMaxValue());
-			throw new FunctionException("Over max value.");
-		}
-		String numberFormat = counterConfig.getNumberFormat();
-		if (StringUtils.isEmpty(numberFormat)) {
-			return String.valueOf(nextValue);
-		}
-		return String.format(Locale.ENGLISH, numberFormat, nextValue);
-	}
+    /**
+     * 需要返回的字符串格式
+     */
+    private String numberFormat;
 
-	@Override
-	public boolean hasNext() {
-		if (!isConfigValid()) {
-			return false;
-		}
-		Integer maxValue = counterConfig.getMaxValue();
-		int nextValue;
-		try {
-			nextValue = nextValue();
-		} catch (ArithmeticException arithmeticException) {
-			LOGGER.error("Get next value fail when counter, reason:{}", arithmeticException.getMessage());
-			return false;
-		}
-		return maxValue == null || nextValue <= maxValue;
-	}
+    @Override
+    public void initConfig(CounterConfig config) {
+        this.counterConfig = config;
+        if (!isConfigValid()) {
+            return;
+        }
 
-	/**
-	 * 通过config配置计算下一个值
-	 *
-	 * @return 下一个值
-	 */
-	protected int nextValue() {
-		ExecuteTimesInfo executeTimesInfo = getExecuteTimesInfo();
-		int nextValue = counterConfig.getSharingMode().getGrinderCountService().nextIncrementNumber(executeTimesInfo);
-		int startValue = counterConfig.getStartValue();
-		int incrementRange = counterConfig.getIncrement();
-		return Math.addExact(Math.multiplyExact(nextValue, incrementRange), startValue);
-	}
+        // 字符串格式
+        numberFormat = config.getNumberFormat();
+        if (numberFormat == null) {
+            numberFormat = "%d";
+        }
+
+        // 通过模拟测试方法执行次数，提前算出该线程能获取的计数器数字放入缓存中
+        int runNumber = 0;
+        int nextNumber;
+        while ((nextNumber = nextValue(runNumber)) <= config.getMaxValue()) {
+            cacheNumber.add(nextNumber);
+            runNumber++;
+        }
+    }
+
+    @Override
+    public boolean isConfigValid() {
+        if (counterConfig == null) {
+            LOGGER.error("The counter config is null.");
+            return false;
+        }
+        return counterConfig.getSharingMode() != null;
+    }
+
+    @Override
+    public String nextNumber() {
+        if (cacheNumber.isEmpty()) {
+            LOGGER.error("No matched number for this thread calculating by config.");
+            throw new FunctionException("No matched number for this thread calculating by config.");
+        }
+        if (cursor < cacheNumber.size()) {
+            return String.format(Locale.ENGLISH, numberFormat, cacheNumber.get(cursor++));
+        }
+        if (counterConfig.resetEachIteration()) {
+            // 走到这里说明当设置了重复取值的时候，重置游标，从第一个开始取值
+            cursor = 0;
+            return String.format(Locale.ENGLISH, numberFormat, cacheNumber.get(cursor));
+        }
+        Integer maxValue = counterConfig.getMaxValue();
+        LOGGER.error("Over max value:{}", maxValue);
+        throw new FunctionException("Over max value:" + maxValue);
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (cursor < cacheNumber.size()) {
+            return true;
+        }
+        return !cacheNumber.isEmpty() && counterConfig.resetEachIteration();
+    }
+
+    /**
+     * 通过config配置计算下一个值
+     *
+     * @return 下一个值
+     */
+    protected int nextValue(int runNumber) {
+        ExecuteTimesInfo executeTimesInfo = getExecuteTimesInfo(runNumber);
+        int nextValue = counterConfig.getSharingMode().getGrinderCountService().nextIncrementNumber(executeTimesInfo);
+        int startValue = counterConfig.getStartValue();
+        int incrementRange = counterConfig.getIncrement();
+        return Math.addExact(Math.multiplyExact(nextValue, incrementRange), startValue);
+    }
 }
